bed7649af2e90783288ab01c9d2785df68dabe49
WenYu Lee <leewen@oregonstate.edu>
2018-03-20 02:02:51 -0700

    Faster version

--- leetcode3.py
+++ leetcode3.py
@@ -1,17 +1,15 @@
 def lengthOfLongestSubstring(self, s):
+    j = 0
     maxnum = 0
-    subnum = 0
-    subset = []
-    for i in s:
-        for index, j in enumerate(subset):
-            if j == i:
-                subset = subset[index + 1:]
-                subnum = len(subset)
-        subset.append(i)
-        subnum += 1
-        maxnum = max(maxnum, subnum)
+    subset = {}
+    for index, i in enumerate(s):
+        if i in subset and subset[i] + 1 > j:
+            j = subset[i] + 1
+        subset[i] = index
+        maxnum = max(maxnum, index - j + 1)
     return maxnum
 
 
 if __name__ == '__main__':
     print(lengthOfLongestSubstring('self', "abcabcbb"))
+    print(lengthOfLongestSubstring('self', "abba"))
