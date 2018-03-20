cb004e4b1accfe3c9c646f405a1c058f0a845bdc
WenYu Lee <leewen@oregonstate.edu>
2018-03-20 16:04:56 -0700

    leetcode No.7

---
+++ leetcode7.py
@@ -0,0 +1,15 @@
+def reverse(self, x):
+    result, neg = 0, False
+    x, neg = abs(x), True if x < 0 else 0
+    while x != 0:
+        result *= 10
+        result += x % 10 if not(x % 10 == 0 and result == 0) else 0
+        x //= 10
+    if abs(x) >= 2 ** 31:
+        return 0
+    return -result if neg else result
+
+
+if __name__ == '__main__':
+    print(reverse('self', 120))
+    print(reverse('self', -123))
