292e9689dc2a3ad985a8c20e93a44375edd934e5
WenYu Lee <leewen@oregonstate.edu>
2018-06-03 23:31:14 -0700

    leetcode NO.118

---
+++ leetcode118.py
@@ -0,0 +1,21 @@
+def generate(self, numRows):
+    result = []
+    if numRows == 0:
+        return result
+    result.append([1])
+    for i in range(0, numRows - 1):
+        pascalNow = []
+        for (index, value) in enumerate(result[-1]):
+            if index == 0:
+                pascalNow.append(1)
+            else:
+                pascalNow.append(result[-1][index - 1] + result[-1][index])
+        pascalNow.append(1)
+        result.append(pascalNow)
+    return result
+
+
+if __name__ == '__main__':
+    print(generate("self", 0))
+    print(generate("self", 1))
+    print(generate("self", 5))
