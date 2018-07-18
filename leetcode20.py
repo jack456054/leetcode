fffcb03a89d2419fdee7ec12d14ea2dd578d5aeb
jack456054 <jack456054@hotmail.com>
2018-07-18 16:53:37 +0800

    Faster version

--- leetcode20.py
+++ leetcode20.py
@@ -1,24 +1,34 @@
 def isValid(self, s):
-    result = []
+    stack, map = [], {'}': '{', ']': '[', ')': '('}
     for par in s:
-        if par == '{' or par == '[' or par == '(':
-            result.append(par)
-        elif par == '}':
-            if result and result[-1] == '{':
-                result.pop()
-            else:
-                return False
-        elif par == ']':
-            if result and result[-1] == '[':
-                result.pop()
-            else:
-                return False
-        elif par == ')':
-            if result and result[-1] == '(':
-                result.pop()
-            else:
-                return False
-    return not result
+        if par not in map:
+            stack.append(par)
+        elif not stack or map[par] != stack.pop():
+            return False
+    return not stack
+
+    # Old version:
+
+    # result = []
+    # for par in s:
+    #     if par == '{' or par == '[' or par == '(':
+    #         result.append(par)
+    #     elif par == '}':
+    #         if result and result[-1] == '{':
+    #             result.pop()
+    #         else:
+    #             return False
+    #     elif par == ']':
+    #         if result and result[-1] == '[':
+    #             result.pop()
+    #         else:
+    #             return False
+    #     elif par == ')':
+    #         if result and result[-1] == '(':
+    #             result.pop()
+    #         else:
+    #             return False
+    # return not result
 
 
 if __name__ == '__main__':
