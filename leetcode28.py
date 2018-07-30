acd13f1dcf7dcd69d23bc3b069c4fd669dcd9086
jack456054 <jack456054@hotmail.com>
2018-07-30 17:24:49 +0800

    leetcode No.28

---
+++ leetcode28.py
@@ -0,0 +1,7 @@
+def strStr(self, haystack, needle):
+    return haystack.find(needle)
+
+
+if __name__ == '__main__':
+    print(strStr('self', "hello", "ll"))
+    print(strStr('self', "aaaaa", "bba"))
