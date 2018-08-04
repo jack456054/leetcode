d6767dc9075c5c05f032a20462568affdd582d1b
jack456054 <jack456054@hotmail.com>
2018-08-05 01:42:18 +0800

    leetcode No.67

---
+++ leetcode67.py
@@ -0,0 +1,25 @@
+def addBinary(self, a, b):
+    binary = int(a) + int(b)
+    currentBit = 0
+    result = ""
+    if not binary:
+        return "0"
+    while binary:
+        currentBit = binary % 10
+        if currentBit == 2:
+            binary += 10
+            result = "0" + result
+        elif currentBit == 3:
+            binary += 10
+            result = "1" + result
+        else:
+            result = str(currentBit) + result
+        binary //= 10
+    return result
+
+
+if __name__ == '__main__':
+    print(addBinary('self', "11", "1"))
+    print(addBinary('self', "1010", "1011"))
+    print(addBinary('self', "0", "0"))
+    print(addBinary('self', "1111", "1111"))
