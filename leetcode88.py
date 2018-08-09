5944ad35285e7faff1967579e44e549733a7ed98
jack456054 <jack456054@hotmail.com>
2018-08-09 16:39:59 +0800

    leetcode No.88

---
+++ leetcode88.py
@@ -0,0 +1,10 @@
+def merge(self, nums1, m, nums2, n):
+    for index in range(m, m + n):
+        nums1[index] = nums2[index - m]
+    nums1.sort()
+
+
+if __name__ == '__main__':
+    print(merge("self", [1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
+    # print(merge("self", 1))
+    # print(merge("self", 5))
