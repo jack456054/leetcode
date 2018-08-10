dd838af18709e344227d992037dba799dcc02ba0
jack456054 <jack456054@hotmail.com>
2018-08-10 23:39:54 +0800

    leetcode No.104

---
+++ leetcode104.py
@@ -0,0 +1,13 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        return 1 + max(Solution.maxDepth(self, root.left), Solution.maxDepth(self, root.right))
