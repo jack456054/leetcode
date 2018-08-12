7255b18a49351dc6b620b5d528a463cad1294679
jack456054 <jack456054@hotmail.com>
2018-08-13 00:38:57 +0800

    leetcode No.107

--- leetcode107.py
+++ leetcode107.py
@@ -5,8 +5,22 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution:
     def levelOrderBottom(self, root):
-        if not root:
-            return None
-        return [Solution.levelOrderBottom(self, root.left)] +[Solution.levelOrderBottom(self, root.right)] + [root.val]
+        result = []
+        level = 0
+        if root:
+            Solution._levelOrderBottom(root, level, result)
+        result.reverse()
+        return result
+
+    def _levelOrderBottom(root, level, result):
+        if len(result) <= level:
+            result.append([])
+        if root:
+            result[level].append(root.val)
+        if root.left:
+            Solution._levelOrderBottom(root.left, level + 1, result)
+        if root.right:
+            Solution._levelOrderBottom(root.right, level + 1, result)
