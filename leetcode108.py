50b20a80e9f5bf7e19fe952e17a37cc549257e45
jack456054 <jack456054@hotmail.com>
2018-08-13 22:32:39 +0800

    leetcode No.108

---
+++ leetcode108.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def sortedArrayToBST(self, nums):
+        if not nums:
+            return None
+        length = len(nums)
+        result = TreeNode(nums[length // 2])
+        result.left = self.sortedArrayToBST(nums[:length // 2])
+        result.right = self.sortedArrayToBST(nums[length // 2 + 1:])
+        return result
