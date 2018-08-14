788c53382a9c0a54a489c8cf7c40ce5b3149dbe7
jack456054 <jack456054@hotmail.com>
2018-08-14 22:25:13 +0800

    leetcode No.110

---
+++ leetcode110.py
@@ -0,0 +1,12 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isBalanced(self, root):
+        if not root:
+            return 0
+        return 1 + max(self.isBalanced(root.left), self.isBalanced(root.right))
