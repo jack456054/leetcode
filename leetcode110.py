d1d37a1bdd8e4f2179169b6b464498521ce24bb4
jack456054 <jack456054@hotmail.com>
2018-08-16 15:15:44 +0800

    leetcode No.110

--- leetcode110.py
+++ leetcode110.py
@@ -8,6 +8,15 @@
 
 class Solution:
     def isBalanced(self, root):
+        balanced = True
+        self._isBalanced(root, balanced)
+        return balanced
+
+    def _isBalanced(self, root, balanced):
         if not root:
             return 0
-        return 1 + max(self.isBalanced(root.left), self.isBalanced(root.right))
+        left = self._isBalanced(root.left, balanced)
+        right = self._isBalanced(root.right, balanced)
+        if abs(left - right) >= 2:
+            balanced = False
+        return 1 + max(left, right)
