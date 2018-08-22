6a711b03f71828d9ab3f445af3f65ffc7e1cbeab
jack456054 <jack456054@hotmail.com>
2018-08-22 13:58:55 +0800

    leetcode No.125

---
+++ leetcode125.py
@@ -0,0 +1,12 @@
+import re
+
+
+class Solution:
+    def isPalindrome(self, s):
+        result = re.findall(r'\w', s.lower())
+        print(re.findall(r"\w+", s.lower()))
+        return result == list(reversed(result))  # Or result[::-1]
+
+
+if __name__ == '__main__':
+    print(Solution.isPalindrome("self", "A man, a plan, a canal: Panama"))
