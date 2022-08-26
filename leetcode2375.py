c16ab8de6371b221962ec6a8e25a66497e63b551
Yoyo <jack456054@hotmail.com>
2022-08-26 17:27:30 +0800

    feat: leetcode no.2375

---
+++ leetcode2375.py
@@ -0,0 +1,26 @@
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        length = len(pattern)
+        result = [0] * (length + 1)
+        for i in range(length + 1):
+            result[i] = i + 1
+        conti = 1
+        last_c = 'I'
+        for index, value in enumerate(pattern):
+            if value == 'D':
+                if last_c == 'D':
+                    conti += 1
+                else:
+                    conti = 2
+                last_c = 'D'        
+            else:
+                if last_c == 'D':
+                    result[index + 1 - conti:index + 1] = result[index + 1- conti:index + 1][::-1]
+                last_c = 'I'
+        if last_c == 'D':
+            result[-(conti):] = result[-(conti):][::-1]
+        return ''.join(map(str, result))
+                    
+        
+            
+            
