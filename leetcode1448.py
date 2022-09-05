79c46a164334f7953190399dde43156e8246ec63
Yoyo <jack456054@hotmail.com>
2022-09-05 22:44:39 +0800

    ✨ Feat: leecode no.1448

---
+++ leetcode1448.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.count = 0
+        self._goodNodes(root, float(-inf))
+        return(self.count)
+        
+    def _goodNodes(self, root: TreeNode, max_num: float):
+        if root.val >= max_num:
+            self.count += 1
+            max_num = root.val
+        if root.left:
+            self._goodNodes(root.left, max_num)
+        if root.right:
+            self._goodNodes(root.right, max_num)
+            
