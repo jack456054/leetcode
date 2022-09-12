e0e3afdc910688b61c0c5e409892420b1d88b18e
Yoyo <jack456054@hotmail.com>
2022-09-12 13:26:15 +0800

    ✨ Feat: leetcode no.1383

--- leetcode1383.py
+++ leetcode1383.py
@@ -1,20 +1,33 @@
 import heapq
 from typing import List
 
+# class Solution:
+#     def maxPerformance(self, n, speed, efficiency, k):
+#         people = sorted(zip(speed, efficiency), key=lambda x: -x[1])
+#         result, sum_speed = 0, 0
+#         min_heap = []
+#         for i, (s, e) in enumerate(people):
+#             if i < k:
+#                 sum_speed += s
+#                 heapq.heappush(min_heap, s)
+#             elif s > min_heap[0]:
+#                 sum_speed += s - heapq.heappushpop(min_heap, s)
+#             else:
+#                 continue
+#             result = max(result, sum_speed * e)
+#         return result % 1000000007
+
 
 class Solution:
     def maxPerformance(self, n, speed, efficiency, k):
         people = sorted(zip(speed, efficiency), key=lambda x: -x[1])
         result, sum_speed = 0, 0
-        min_heap = []
-        for i, (s, e) in enumerate(people):
-            if i < k:
-                sum_speed += s
-                heapq.heappush(min_heap, s)
-            elif s > min_heap[0]:
-                sum_speed += s - heapq.heappushpop(min_heap, s)
-            else:
-                continue
+        max_speed = []
+        for _, (s, e) in enumerate(people):
+            if len(max_speed) > k - 1:
+                sum_speed -= heapq.heappop(max_speed)
+            sum_speed += s
+            heapq.heappush(max_speed, s)
             result = max(result, sum_speed * e)
         return result % 1000000007
 
