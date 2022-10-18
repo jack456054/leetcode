44dbd685cccab2e3bb8dd7dea3621f6f90ec4eec
Yoyo <jack456054@hotmail.com>
2022-10-18 17:03:05 +0800

    ✨ Feat: leetcode no.38

--- leetcode38.py
+++ leetcode38.py
@@ -16,6 +16,32 @@ def countAndSay(self, n):
     return seq
 
 
+# class Solution:
+#     def countAndSay(self, n: int) -> str:
+#         pattern = '1'
+#         while n > 1:
+#             last_c = pattern[0]
+#             count = 1
+#             next_pattern = ''
+#             for c in pattern[1:]:
+#                 if c == last_c:
+#                     count += 1
+#                 else:
+#                     next_pattern += f'{count}{last_c}'
+#                     count = 1
+#                     last_c = c
+#             pattern = next_pattern + f'{count}{last_c}'
+#             n -= 1
+#         return pattern
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        return '1' if n == 1 else self._countAndSay(n - 1, '1')
+
+    def _countAndSay(self, n: int, pattern: str) -> str:
+        results = ''.join([f'{len(list(g))}{k}' for k, g in groupby(pattern)])
+        return results if n == 1 else self._countAndSay(n - 1, results)
+
+
 if __name__ == '__main__':
     print(countAndSay('self', 1))
     print(countAndSay('self', 4))
