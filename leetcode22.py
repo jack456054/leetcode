eb2dbf2d5d57a59d94d25040cdcc6d78fdf4cef7
Yoyo <jack456054@hotmail.com>
2022-10-29 00:33:13 +0800

    ✨ Feat: leetcode no.22

--- leetcode22.py
+++ leetcode22.py
@@ -1,7 +1,10 @@
-def generateParenthesis(self, n):
-    count = 0
-    if 
-
-
-if __name__ == '__main__':
-    print(generateParenthesis('self', 3))
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        results = []
+        if n == 0: 
+            return ['']
+        for c in range(n):
+            for i in self.generateParenthesis(c):
+                for j in self.generateParenthesis(n - c - 1):
+                    results.append(f'({i}){j}')
+        return results
